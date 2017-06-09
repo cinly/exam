da5f94606054405d6a3750972852f63b7ce9fc9e
cinly <cinlyma@gmail.com>
2017-06-09 16:07:11 +0800

    fix date of next year collage entrance exam

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,11 +2,11 @@ class Question < ApplicationRecord
   mount_uploader :imagea, ImageaUploader
   mount_uploader :imageb, ImagebUploader
   mount_uploader :imagec, ImagecUploader
-  mount_uploader :imaged, ImagedUploader 
+  mount_uploader :imaged, ImagedUploader
   belongs_to :quiz
   has_many :answers
 
-  validates :content, presence: true
+  validates :content,  presence: true
   validates :answer_1, presence: true
   validates :answer_2, presence: true
   validates :answer_3, presence: true
